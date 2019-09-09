6dd5b83937769cdcd0ec42ea1fd9fa457b93e15a
Pierre KORE <pierre.kore@infeeny.com>
2019-09-09 17:03:28 +0200

    some useless code

--- ExercicesReflection/Program.cs
+++ ExercicesReflection/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
-//using Models;
+using Models;
 
 namespace Constructor
 {
@@ -10,24 +11,28 @@ namespace Constructor
         {
             //Person p = new Person();
             //Assembly a = typeof(Program).Assembly;
-            //var assembly = a.GetReferencedAssemblies();
+            //var referencedAssemblies = a.GetReferencedAssemblies();
             //Console.WriteLine("a :{0}", a);
-            //Console.WriteLine("assembly : {0}", assembly);
-            //var person = new Person
+            //Assembly ReflectionData = null;
+            //foreach (AssemblyName assembly in referencedAssemblies)
             //{
-            //    Age = 25,
-            //    Name = "Paul",
-            //    NumeroDeSecu = "0123456789"
-            //};
-
-            //Type objType = person.GetType();
-            //PropertyInfo[] properties = objType.GetProperties();
-
-            //Console.WriteLine("type of Object: {0}", objType);
-            //foreach (PropertyInfo property in properties)
-            //{
-            //   Console.WriteLine("{0}: {1}", property, property.GetValue(person));
+            //    if (assembly.Name.Equals("ReflectionData"))
+            //        ReflectionData = Assembly.Load(assembly.Name);
+            //    Console.WriteLine("assembly : {0}", assembly);
             //}
+
+            //Type PersonType = ReflectionData.GetTypes().First();
+
+            //object ClassInstance = Activator.CreateInstance(PersonType);
+
+            //PropertyInfo property = PersonType.GetProperty("Age");
+            //MethodInfo method = PersonType.GetMethod("IncrementAge");
+
+
+            //Console.WriteLine("{0}: {1}", property.Name, PersonType.GetProperty("Age").GetValue(ClassInstance));
+            //Console.WriteLine("Apply method {0}", method.Name);
+            //method.Invoke(ClassInstance, null);
+            //Console.WriteLine("Age: {0}", PersonType.GetProperty("Age").GetValue(ClassInstance));
         }
     }
 }
