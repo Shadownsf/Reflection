c51945d2035be13ceb4563a6ff674b3dad07578a
Pierre KORE <pierre.kore@infeeny.com>
2019-09-11 09:24:53 +0200

    Clean niveau 1 et 2

--- ExercicesReflection/Exercices_Lvl_1.cs
+++ ExercicesReflection/Exercices_Lvl_1.cs
@@ -9,18 +9,16 @@ namespace ExercicesReflection
 {
     public class Exercices_Lvl_1
     {
-
         [Custom("Method_ReturnName")]
         public static string ReturnName(object o)
         {
             Type type = o.GetType();
-            
+
             var name = type.GetProperty("Name").GetValue(o).ToString();
             return name;
         }
 
-        [Custom("Method_returnAgeincremented")]
-        public static int returnAgeincremented(object o)
+        public static int returAgeincremented(object o)
         {
             Type type = o.GetType();
 
@@ -30,6 +28,59 @@ namespace ExercicesReflection
             //fait un appel a la method getMethod
             var age = (int)method.Invoke(o, null);
             return age;
+        }
+
+
+        [Custom("Method_SetName")]
+        public static Type SetName(object o, string s)
+        {
+            Type type = o.GetType();
+
+            type.GetProperty("Name").SetValue(o, s);
+            return type;
+        }
+
+        [Custom("Method_GetMethod")]
+        public static MethodInfo GetMethod(object o, string s)
+        {
+            Type type = o.GetType();
+
+            MethodInfo method = type.GetMethod(s);
+            return method;
+        }
+
+        [Custom("Method_GetReturnType")]
+        public static Type GetReturnType(object o, string s)
+        {
+            Type type = o.GetType();
+
+            MethodInfo method = type.GetMethod(s);
+            return method.ReturnType;
+        }
+
+        [Custom("Method_GetMethodParameters")]
+        public static ParameterInfo[] GetMethodParameters(object o, string s)
+        {
+            Type type = o.GetType();
+
+            MethodInfo method = type.GetMethod(s);
+            return method.GetParameters();
+        }
+
+        [Custom("Method_GetBaseType")]
+        public static Type GetBaseType(object o)
+        {
+            Type type = o.GetType();
+
+            return type.BaseType;
+        }
+
+        [Custom("Method_GetInterface")]
+        public static Type GetInterface(object o, string s)
+        {
+            Type type = o.GetType();
+
+            return type.GetInterface(s);
         }
     }
 }
--- Tests/Tests_Lvl_1.cs
+++ Tests/Tests_Lvl_1.cs
@@ -30,7 +30,7 @@ namespace Tests
         public void t5_sets_name()
         {
             Person person = new Person();
-            Type type = Exercice1.SetName(person, "Titi");
+            Type type = Exercices_Lvl_1.SetName(person, "Titi");
             Assert.That(person.Name, Is.EqualTo("Titi"));
         }
 
@@ -38,7 +38,7 @@ namespace Tests
         public void t6_gets_method()
         {
             Person person = new Person();
-            MethodInfo method = Exercice1.GetMethod(person, "IncrementAge");
+            MethodInfo method = Exercices_Lvl_1.GetMethod(person, "IncrementAge");
             method.Invoke(person, null);
             Assert.That(person.Age, Is.EqualTo(16));
         }
@@ -47,7 +47,7 @@ namespace Tests
         public void t7_gets_return_type()
         {
             Person person = new Person();
-            Type type = Exercice1.GetReturnType(person, "IncrementAge");
+            Type type = Exercices_Lvl_1.GetReturnType(person, "IncrementAge");
             Assert.That(type, Is.EqualTo(typeof(int)));
         }
 
@@ -55,7 +55,7 @@ namespace Tests
         public void t8_gets_method_parameters()
         {
             Person person = new Person();
-            ParameterInfo[] parameters = Exercice1.GetMethodParameters(person, "SetAge");
+            ParameterInfo[] parameters = Exercices_Lvl_1.GetMethodParameters(person, "SetAge");
             Assert.That("" + parameters[0], Is.EqualTo("Int32 age"));
         }
 
@@ -63,7 +63,7 @@ namespace Tests
         public void t9_gets_base_type()
         {
             Person person = new Person();
-            Type type = Exercice1.GetBaseType(person);
+            Type type = Exercices_Lvl_1.GetBaseType(person);
             Assert.That(type, Is.EqualTo(typeof(object)));
         }
 
@@ -71,7 +71,7 @@ namespace Tests
         public void tA_gets_interface()
         {
             Person person = new Person();
-            Type theInterface = Exercice1.GetInterface(person, "IncrementAge");
+            Type theInterface = Exercices_Lvl_1.GetInterface(person, "IncrementAge");
             Assert.That(theInterface, Is.EqualTo(null));
         }
 
